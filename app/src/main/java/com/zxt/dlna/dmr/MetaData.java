b9a311195df5d9e0827f1be58a576ffc3765135a
jjm2473 <jjm2473@gmail.com>
2017-08-20 22:18:49 +0800

    improve meta data parser.

--- app/src/main/java/com/zxt/dlna/dmr/MetaData.java
+++ app/src/main/java/com/zxt/dlna/dmr/MetaData.java
@@ -6,49 +6,78 @@ import org.xml.sax.Attributes;
 import org.xml.sax.SAXException;
 import org.xml.sax.helpers.DefaultHandler;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Created by lantern on 2017/8/13.
  */
 
 public class MetaData {
+    private static enum State {
+        IGNORE,
+        TYPE,
+        NAME,
+        ARTIST,
+        ALBUM,
+        PICTURE
+    }
+    private static final Map<String, State> States = new HashMap<String, State>(){
+        {
+            put("upnp:class", State.TYPE);
+            put("dc:title", State.NAME);
+            put("dc:creator", State.ARTIST);
+            put("upnp:artist", State.ARTIST);
+            put("upnp:album", State.ALBUM);
+            put("upnp:albumArtURI", State.PICTURE);
+        }
+    };
     public String name;
     public String type;
+    public String artist;
+    public String album;
+    public String picture;
 
     public static MetaData parse(String metaData) throws SAXException {
         final MetaData data = new MetaData();
         Xml.parse(metaData, new DefaultHandler(){
-            int state = 0;
+            State state = State.IGNORE;
             @Override
             public void startElement(String uri, String localName, String qName, Attributes attributes) throws SAXException {
-                state = 0;
-                if("dc:title".equals(qName)){
-                    state = 1;
-                }
-                if("upnp:class".equals(qName)){
-                    state = 2;
+                state = State.IGNORE;
+                if(qName != null){
+                    State state0 = States.get(qName);
+                    if(state0 != null){
+                        state = state0;
+                    }
                 }
             }
 
             @Override
             public void characters(char[] ch, int start, int length) throws SAXException {
-                if(state != 0){
+                if(state != State.IGNORE){
                     String text = new String(ch, start, length);
                     switch (state){
-                        case 1:
+                        case NAME:
                             data.name = text;
                             break;
-                        case 2:
-                            switch (text){
-                                case "object.item.videoItem":
-                                    data.type = MediaType.VIDEO;
-                                    break;
-                                case "object.item.imageItem":
-                                    data.type = MediaType.IMAGE;
-                                    break;
-                                case "object.item.audioItem":
-                                    data.type = MediaType.AUDIO;
-                                    break;
+                        case TYPE:
+                            if (text.startsWith("object.item.videoItem")) {
+                                data.type = MediaType.VIDEO;
+                            } else if (text.startsWith("object.item.imageItem")) {
+                                data.type = MediaType.IMAGE;
+                            } else if (text.startsWith("object.item.audioItem")) {
+                                data.type = MediaType.AUDIO;
                             }
+                            break;
+                        case ARTIST:
+                            data.artist = text;
+                            break;
+                        case ALBUM:
+                            data.album = text;
+                            break;
+                        case PICTURE:
+                            data.picture = text;
                             break;
                     }
                 }
